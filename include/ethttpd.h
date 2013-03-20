c695387bb50df2a2e7640aee704305af4b753a4b
Cedric Porter <cedricporter@gmail.com>
2013-03-20 23:22:26 +0800

    head file

--- include/ethttpd.h
+++ include/ethttpd.h
@@ -1,15 +1,27 @@
 /**
  * @file   ethttpd.h
- * @author Hua Liang[Stupid ET] 
- * 
- * @brief  
- * 
- * 
+ * @author Hua Liang[Stupid ET]
+ *
+ * @brief
+ *
+ *
  */
 
 #ifndef ETHTTPD_H
 #define ETHTTPD_H
 
-#define HUALIANG "hualiang!"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+
+#define MAXLINE 4096
 
 #endif /* ETHTTPD_H */
