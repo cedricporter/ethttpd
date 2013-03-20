a9c13a7271c02ff67f449601e7d02d74af258ccf
Cedric Porter <cedricporter@gmail.com>
2013-03-20 21:23:40 +0800

    First commit

---
+++ include/ethttpd.h
@@ -0,0 +1,15 @@
+/**
+ * @file   ethttpd.h
+ * @author Hua Liang[Stupid ET] 
+ * 
+ * @brief  
+ * 
+ * 
+ */
+
+#ifndef ETHTTPD_H
+#define ETHTTPD_H
+
+#define HUALIANG "hualiang!"
+
+#endif /* ETHTTPD_H */
---
+++ src/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file   main.c
+ * @author Hua Liang[Stupid ET]
+ *
+ * @brief
+ *
+ *
+ */
+
+#include "ethttpd.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!" HUALIANG);
+
+    return 0;
+}
