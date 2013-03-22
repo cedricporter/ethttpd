0bc49dfa907e335ea0c10587b4bd324f521a79d2
Cedric Porter <cedricporter@gmail.com>
2013-03-22 13:57:00 +0800

    new log function et_log

--- Makefile
+++ Makefile
@@ -1,14 +1,13 @@
 all: ethttpd
 
 ethttpd:
-	(cd src; $(MAKE)) && cp src/ethttpd .
+	(cd src; $(MAKE))
 
 clean:
 	rm ethttpd
 	(cd src; $(MAKE) clean)
 
 .PHONY: ethttpd
-
 
 test: src/main.c
 	gcc -I include -MM -MT $^ $<
--- src/main.c
+++ src/main.c
@@ -10,9 +10,17 @@
 #include "ethttpd.h"
 #include <time.h>
 #include <netdb.h>
-
+#include <stdarg.h>
 
 #define ETHTTPD_PORT 12345
+
+void et_log(char *s, ...)
+{
+	va_list ap;
+	va_start(ap,s);
+    vfprintf(stderr, s, ap);
+	va_end(ap);
+}
 
 int initialize(int port)
 {
@@ -35,11 +43,13 @@ int initialize(int port)
 
     if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
     {
+        et_log("Bind error");
         exit(1);
     }
 
     if (listen(listenfd, 100) < 0)
     {
+        et_log("listen error");
         exit(1);
     }
 
@@ -55,19 +65,43 @@ static void child_reaper(int sig)
 		printf("Child process %d exited with status %d\n",pid,status);
 }
 
+int handle(int connfd)
+{
+    char read_buff[MAXLINE];
+    time_t ticks;
+    int n;
+    char buff[MAXLINE];
+        
+    /* read */
+    n = read(connfd, read_buff, MAXLINE);
+
+    /* write back */
+    ticks = time(NULL);
+    snprintf(buff, sizeof(buff),
+             "HTTP/1.0 200 OK\r\n"
+             "Content-Type: text/plain\r\n"
+             "\r\n"
+             "%.24s\r\n\r\n%s", ctime(&ticks), read_buff);
+    printf("fputs\n");
+    n = write(connfd, buff, strlen(buff));
+    if (n < 0)
+    {
+        exit(1);
+    }
+    close(connfd);
+
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     int listenfd, connfd;
-    char buff[MAXLINE];
-    time_t ticks;
-    int n, pid;
-    FILE *fh;
+    int pid;
     struct sockaddr_in peeraddr;
 
-	struct sigaction sa = {
-		.sa_handler = &child_reaper,
-		.sa_flags = SA_NOCLDSTOP
-	};
+	struct sigaction sa;
+    sa.sa_handler = &child_reaper;
+    sa.sa_flags = SA_NOCLDSTOP;
 
     sigaction(SIGCHLD, &sa, NULL);
 
@@ -78,7 +112,7 @@ int main(int argc, char **argv)
     {
         socklen_t peerlen=sizeof(peeraddr);
 
-        printf("Waiting...\n");
+        et_log("Waiting...\n");
         connfd = accept(listenfd, (struct sockaddr *)&peeraddr, &peerlen);
         if (connfd < 0)
         {
@@ -86,7 +120,7 @@ int main(int argc, char **argv)
                 printf("accept error: %s", strerror(errno));
             continue;
         }
-        printf("accept\n");
+        et_log("accept\n");
 
         pid = fork();
 
@@ -95,24 +129,7 @@ int main(int argc, char **argv)
         case 0:
             close(listenfd);
 
-            /* read */
-            n = read(connfd, buff, MAXLINE);
-
-            /* write back */
-            ticks = time(NULL);
-            snprintf(buff, sizeof(buff),
-                     "HTTP/1.0 200 OK\r\n"
-                     "Content-Type: text/plain\r\n"
-                     "\r\n"
-                     "\r\n%.24s\r\n", ctime(&ticks));
-            printf("fputs\n");
-            n = write(connfd, buff, strlen(buff));
-            if (n < 0)
-            {
-                exit(1);
-            }
-            close(connfd);
-            exit(0);
+            exit(handle(connfd));
             break;
         default:
             close(connfd);
