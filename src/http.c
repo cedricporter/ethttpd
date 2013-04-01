413fb6389a3b6709f3cc61e975c48ca1e5b46829
Cedric Porter <cedricporter@gmail.com>
2013-04-01 18:52:18 +0800

    parse http

--- src/http.c
+++ src/http.c
@@ -10,7 +10,116 @@
 #include "ethttpd.h"
 #include "http.h"
 #include "utils.h"
+#include <ctype.h>
 
+typedef struct request
+{
+    int method;
+} HttpRequest;
+
+typedef enum {
+    M_INVALID,
+    M_GET,
+    M_POST
+} reqtype;
+
+static reqtype check_reqtype(char *req)
+{
+	if(!strncmp("GET",req,3)) return M_GET;
+	if(!strncmp("POST",req,4)) return M_POST;
+
+    return M_INVALID;
+}
+
+static int parse_header(char *buf)
+{
+    reqtype method;
+    char *uri, *doc, *p;
+    char *uri_start, *uri_end;
+    char *protocol_start, *protocol_end;
+    char ch;
+
+    enum {
+        sw_start = 0,
+        sw_method,
+        sw_spaces_before_uri,
+        sw_uri,
+        sw_spaces_before_protocol,
+        sw_protocol,
+        sw_key,
+        sw_value
+    } state;
+
+    p = buf;
+    state = sw_start;
+
+    while (*p)
+    {
+        ch = *p;
+        switch (state)
+        {
+        case sw_start:
+        case sw_method:
+            switch (method = check_reqtype(buf))
+            {
+            case M_GET:
+                p += 3;
+                break;
+            case M_POST:
+                p += 4;
+                break;
+            default:
+                exit(-1);
+            }
+            state = sw_spaces_before_uri;
+            break;
+        case sw_spaces_before_uri:
+            if (isspace(ch))
+            {
+                p++;
+            }
+            else
+            {
+                state = sw_uri;
+                uri_start = p;
+            }
+            break;
+        case sw_uri:
+            if (!isspace(ch))
+                p++;
+            else
+            {
+                uri_end = p;
+                state = sw_spaces_before_protocol;
+            }
+            break;
+        case sw_spaces_before_protocol:
+            if (isspace(ch))
+            {
+                p++;
+            }
+            else
+            {
+                state = sw_protocol;
+                protocol_start = p;
+            }
+            break;
+        case sw_protocol:
+            if (!isspace(ch))
+            {
+                p++;
+            }
+            else
+            {
+                protocol_end = p;
+            }
+        }
+
+    }
+
+
+    return 0;
+}
 
 int handle_request(int connfd)
 {
@@ -20,6 +129,8 @@ int handle_request(int connfd)
 
     /* read */
     n = read(connfd, read_buff, MAXLINE);
+
+
 
     /* write back */
     snprintf(buff, sizeof(buff),
